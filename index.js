f68213f7454ad50595dd831259fabd3f2e156ca3
DolimovS <samandardolimov71@gmail.com>
2026-02-06 01:08:02 +0500

    fds

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ bot.onText(/\/start/, msg => {
     const chatId = msg.chat.id
     bot.sendMessage(chatId, `Assalomu alaykum ${msg.chat.first_name} botga xush kelibsiz!`,
         {
-            reply_markup: {
-                inline_keyboard: [
+            reply_markup: { //shu joyda tugmalarni beramiz
+                inline_keyboard: [ //tugmalar massivini beramiz va har bir tugma yangi qatorga tushadi
                     [
                         {
                             text: "DolimovS",
@@ -23,10 +23,16 @@ bot.onText(/\/start/, msg => {
                         {
                             text: "Admin bilan bog'lanish",
                             url: "https://t.me/Dolimov_9"
-                        },
+                        }, // bitta qatorga ikkita tugma
                         {
                             text: "O'zim haqimda",
                             callback_data: "bio"
+                        }
+                    ],
+                    [
+                        {
+                            text:"Test",
+                            callback_data:"test"
                         }
                     ]
                 ]
@@ -40,6 +46,23 @@ bot.on('callback_query', (query) => {
     const chatId = query.message.chat.id
     if (query.data == "bio") {
         bot.sendMessage(chatId, `Men Dolimov Samandar, 2005-yil 9-avgustda tug'ilganman. Hozirda 2026-yilda 21 yoshdaman. Dasturlashga qiziqaman va turli loyihalar ustida ishlayman. Mening maqsadim dasturlash sohasida o'z bilimlarimni oshirish va yangi texnologiyalarni o'rganishdir.`)
+    }else if (query.data=="error"){  // xatolik yuz beradigan tugma
+        bot.answerCallbackQuery(query.id, {
+            text:"Xatolik yuz berdi!",
+            show_alert:true,  // show_alert true bo'lsa alert ko'rinadi, false bo'lsa pastda kichik habar ko'rinadi
+        })
+    }
+    else{
+        bot.answerCallbackQuery(query.id, {
+            text:"Bu tugma hali ishlamayapti!",
+            show_alert:false,  // show_alert true bo'lsa alert ko'rinadi, false bo'lsa pastda kichik habar ko'rinadi
+        })  // agar xabar yuborilmasa quyidagi usul bilan ham yuborish mumkin yoki boshqa xatoliglar chiqsa 
+        .then(()=>{
+            bot.sendMessage(chatId, "Bu tugma hali ishlamayapti!")
+        })  
+        .catch((err)=>{
+            console.log(err);
+        })
     }
 })
 
